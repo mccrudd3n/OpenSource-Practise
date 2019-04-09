364ef06f9314d003b12edacd1635b8b53016ed64
Griffin <49352991+mccrudd3n@users.noreply.github.com>
2019-04-10 00:07:12 +0100

    Update Chapter3.2.cpp
    Missed out on assigning an initial value of Zero to the variable

--- c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chapter3.2.cpp
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chapter3.2.cpp
@@ -8,12 +8,12 @@ int main()
 
     //Ask user for number 1 =x
     cout << "Enter your First Number: " << endl;
-    int x;
+    int x = 0;
     cin >> x;
 
     // Ask usr for num 2 = y
     cout << "Enter your Second Number: " << endl;
-    int y;
+    int y = 0;
     cin >> y;
     //define total and maths is done then printed
     int total;
