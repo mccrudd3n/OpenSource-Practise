8838c6d4463cfafd2d79a47a4e0a6d0759470065
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-15 16:58:42 +0100

    Note: It is fairly each to remeber that "(Raining &&!Transport)" can be read as “Raining AND NO/NOT Transport.”

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_5/Using Logical NOT (!) and Logical AND (&&) Operators in if Statements for Conditional Processing.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int logic()
+{
+    using namespace std;
+
+}
+
+
+int main()
+{
+    using namespace std;
+
+    cout << endl; // Extra space so that its easy readable
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Using Logical NOT (!) and Logical AND (&&) Operators in ifStatements forConditional Processing." << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space so that its easy readable
+
+    //Functions are Invoked here
+    logic();
+
+    return 0;
+}
