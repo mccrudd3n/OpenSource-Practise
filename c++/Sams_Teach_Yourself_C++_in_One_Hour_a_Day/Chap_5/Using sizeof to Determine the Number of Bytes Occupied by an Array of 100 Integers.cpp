50b65ccf27ef258e4664bed5560d630517de6212
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-16 13:31:25 +0100

    The program demonstrates how sizeof is capable of returning the size of an array of 100 integers in bytes, which is 400 bytes.
    The program also demonstrates that the size of each element is 4 bytes. 
    sizeof can be quite useful when you need to dynamically allocate memory 
    for N objects, especially of a type created by yourself. You would use 
    the result of the sizeof operation in determining the amount of memory 
    occupied by each object and then dynamically allocate using the operator 
    new.

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_5/Using sizeof to Determine the Number of Bytes Occupied by an Array of 100 Integers.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+
+int CompoundAssignmentOps()
+{
+    using namespace std;
+
+
+}
+
+
+int main()
+{
+    using namespace std;
+
+    cout << endl; // Extra space so that its easy readable
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Using sizeof to Determine the Number of Bytes Occupied by an Array of 100 Integers,\
+    and That by Each Element Therein." << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space so that its easy readable
+
+    //Functions are Invoked here
+    CompoundAssignmentOps();
+
+    return 0;
+}
