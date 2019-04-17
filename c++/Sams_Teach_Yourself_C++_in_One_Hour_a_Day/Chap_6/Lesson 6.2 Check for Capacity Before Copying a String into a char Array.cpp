ab6aba6bef1b909ae625049db863fb9a94e7ed8b
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-17 17:04:21 +0100

    Lesson 6.2 Check for Capacity Before Copying a String into a char Array.

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_6/Lesson 6.2 Check for Capacity Before Copying a String into a char Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+int ConditionalExe()
+{
+    using namespace std;
+    char Buffer [20] = {'\0'}
+    string LineEntered;
+    getline (cin, LineEntered);
+
+
+}
+
+
+int main()
+{
+    using namespace std;
+
+    cout << endl; // Extra space so that its easy readable
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 6.2 Check for Capacity Before Copying a String into a char Array." << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space so that its easy readable
+
+    //Functions are Invoked here
+    ConditionalExe();
+
+    return 0;
+}
