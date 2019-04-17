c4077720ff6b045f35fc3ab8e4b7355bc8e1387c
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-17 16:33:46 +0100

    Lesson 6.1 - Multiplying or Adding Two Integers on Basis of User Input.

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_6/Lesson 6.1 - Multiplying or Adding Two Integers on Basis of User Input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+int ConditionalExe()
+{
+    using namespace std;
+
+}
+
+
+int main()
+{
+    using namespace std;
+
+    cout << endl; // Extra space so that its easy readable
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 6.1 - Multiplying or Adding Two Integers on Basis of User Input." << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space so that its easy readable
+
+    //Functions are Invoked here
+    ConditionalExe();
+
+    return 0;
+}
