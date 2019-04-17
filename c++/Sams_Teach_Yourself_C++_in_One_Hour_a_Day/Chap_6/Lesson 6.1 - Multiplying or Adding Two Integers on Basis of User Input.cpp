ad40cc85403b80f1c832c801e81ee9335ae85af3
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-17 17:04:37 +0100

    Update

--- c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_6/Lesson 6.1 - Multiplying or Adding Two Integers on Basis of User Input.cpp
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_6/Lesson 6.1 - Multiplying or Adding Two Integers on Basis of User Input.cpp
@@ -4,6 +4,24 @@
 int ConditionalExe()
 {
     using namespace std;
+    // Ask for User Input
+
+    cout << "Enter 2 Values: " << endl;
+    int Num1 =0, Num2 =0;
+    cin >> Num1;
+    cin >> Num2;
+
+    cout << "Enter \"m\" to multiply, anything else to add: ";
+    char UserSelection = '\0';
+    cin >> UserSelection;
+
+    int Result =0;
+    if (UserSelection == 'm')
+      Result = Num1 * Num2;
+    else
+      Result = Num1 + Num2;
+
+    cout << "Result is : " << Result << endl;
 
 }
 
