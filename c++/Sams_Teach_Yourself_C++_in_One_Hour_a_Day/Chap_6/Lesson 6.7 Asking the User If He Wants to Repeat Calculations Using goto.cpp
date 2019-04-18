115d538718fc849db6ef7f049b99be6d7ba09d16
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-18 17:42:33 +0100

    Warning, Dont test this with 99999 :')

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_6/Lesson 6.7 Asking the User If He Wants to Repeat Calculations Using goto.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+int ConditionalOp()
+{
+    using namespace std;
+
+    //User Input
+    int Num1 =0, Num2 =0;
+    cout << "Enter Number 1: " << endl;
+    cout << " >> ";
+    cin >> Num1;
+    cout << "Enter Number 2: " << endl;
+    cout << " >> ";
+    cin >> Num2;
+
+    int Max = (Num1 < Num2) ? Num1 : Num2;
+    cout << "The greater of " << Num1 << " and " << Num2 << " is: " << Max << endl;
+
+}
+int main()
+{
+    using namespace std;
+
+    cout << endl; // Extra space so that its easy readable
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 6.7 Asking the User If He Wants to Repeat Calculations Using goto" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space so that its easy readable
+
+    //Functions are Invoked here
+    ConditionalOp();
+
+    return 0;
+}
