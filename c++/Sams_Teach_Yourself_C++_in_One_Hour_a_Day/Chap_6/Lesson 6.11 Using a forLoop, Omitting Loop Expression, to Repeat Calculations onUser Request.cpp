3dbe2dbcb654209250eaa1af43998df52c8be8c5
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-21 17:51:10 +0100

    Note: This is identical to Listing 6.8 that used the whileloop; the only difference is that thisone uses for.

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_6/Lesson 6.11 Using a forLoop, Omitting Loop Expression, to Repeat Calculations onUser Request.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int Function()
+{
+    using namespace std;
+    // without loop expression
+    for (char UserSelection = 'm'; (UserSelection != 'x');)
+    {
+    int Num1 = 0, Num2 = 0;
+    cout << "Enter Number 1:" << endl;
+    cout << " >> ";
+    cin >> Num1;
+    cout << "Enter Number 2:" << endl;
+    cout << " >> ";
+    cin >> Num2;
+
+    cout << Num1 << " x " << Num2 << " = " << Num1 * Num2 << endl;
+    cout << Num1 << " + " << Num2 << " = " << Num1 + Num2 << endl;
+        cout << "Press x to exit or any other key to recalculate" << endl;
+    cin >> UserSelection;
+    }
+    cout << "End of Program." << endl;
+}
+
+int main()
+{
+    using namespace std;
+
+    cout << endl; // Extra space so that its easy readable
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 6.11 Using a forLoop, Omitting Loop Expression, to Repeat Calculations onUser Request" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space so that its easy readable
+
+    //Functions are Invoked here
+    Function();
+
+    return 0;
+}
