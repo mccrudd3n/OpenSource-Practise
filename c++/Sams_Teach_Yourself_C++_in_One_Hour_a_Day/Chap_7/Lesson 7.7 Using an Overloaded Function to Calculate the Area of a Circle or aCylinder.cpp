289e9abcae245595e7aa4ab7d08a5ad4eaf0a478
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-25 19:23:17 +0100

    Note: Both functions have the same names, Area; same return types,double; and different sets of parameters —hence they’re overloaded

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_7/Lesson 7.7 Using an Overloaded Function to Calculate the Area of a Circle or aCylinder.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+const double Pi = 3.14159;
+
+//Function Declaration
+double Area (double Radius);
+double Area (double Radius, double Height);
+
+void QueryAndCalculate()
+    {
+    cout << endl; // Extra space
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 7.7 Using an Overloaded Function to Calculate the Area of a Circle or a Cylinder" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space
+
+    cout << "Enter z for Cylincer, c for cicle: " << endl;
+    cout << " >> ";
+    char Choice = '\0';
+    cin >> Choice;
+
+
+
+
+
+    }
+
+int main()
+{
+  QueryAndCalculate ()
+  return 0;
+}
