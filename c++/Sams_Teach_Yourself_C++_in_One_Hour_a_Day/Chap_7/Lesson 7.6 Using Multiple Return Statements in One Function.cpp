4241e96ea576e1505f2341e3ce991e2aba0cda94
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-25 19:09:43 +0100

    Missing Header

--- c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_7/Lesson 7.6 Using Multiple Return Statements in One Function.cpp
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_7/Lesson 7.6 Using Multiple Return Statements in One Function.cpp
@@ -5,6 +5,12 @@ const double Pi = 3.14159;
 
 void QueryAndCalculate()
     {
+    cout << endl; // Extra space
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 7.5 Using Recursive Functions to Calculate a Number in the Fibonacci Series" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+    cout << endl; // Extra space
+
     cout << "Enter radius: ";
     double Radius = 0;
     cin >> Radius;
