2718716b36bbdf0db1f00e1334b9d4a4c6560071
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-04-26 23:21:22 +0100

    Lesson 7.10 Commit

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_7/Lesson 7.10 Using an Inline Function That Doubles an Integer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+//Inline defined
+inline long DoubleNum (int InputNum)
+{
+  return InputNum * 2;
+}
+
+int main()
+  {
+    cout << endl; // Extra space
+    cout << "-------------------------------------------------------------" << endl;
+    cout << "Lesson 7.10 Using an Inline Function That Doubles an Integer" << endl;
+    cout << "-------------------------------------------------------------" << endl;
+
+    cout << "Enter an Integer: " << endl;
+    cout << " >> ";
+    int InputNum =0;
+    cin >> InputNum;
+
+    //call inline Function
+    cout << "Double is " << DoubleNum(InputNum) << endl;
+
+    return 0;
+
+  }
