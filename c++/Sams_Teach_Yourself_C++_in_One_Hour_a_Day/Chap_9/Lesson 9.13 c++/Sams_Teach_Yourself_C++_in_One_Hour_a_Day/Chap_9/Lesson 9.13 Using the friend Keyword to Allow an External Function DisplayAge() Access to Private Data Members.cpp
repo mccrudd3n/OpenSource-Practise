b59bbe036a399e2d77a5427116f1ab97bcfe5f0a
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-05-23 22:44:25 +0100

    Lesson 9.13 Commit - "Ohh Friends"

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_9/Lesson 9.13 c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_9/Lesson 9.13 Using the friend Keyword to Allow an External Function DisplayAge() Access to Private Data Members.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+private:
+  string Name;
+  int Age;
+  friend void DisplayAge(const Human& Person);
+public:
+  Human(string InputName, int InputAge)
+  {
+    Name = InputName;
+    Age = InputAge;
+  }
+};
+
+void DisplayAge(const Human& Person)
+{
+  cout << Person.Age << endl;
+}
+
+int main()
+{
+  Human FirstMan("Adam", 25);
+  cout << "Accessing private member Age via friend: ";
+  DisplayAge(FirstMan);
+
+  return 0;
+}
