a066336f544da8b3b8dfe2c0eee8d7bb92d3a13c
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-05-31 18:23:20 +0100

    Lesson 10.6 Note:Overriding can potentially hide all overloaded versions of Fish::Swim()even causing compilation failure when theoverloaded ones are used

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_10/Lesson 10.6 Demonstrating That Tuna--Swim()Hides Overloaded Method Fish--Swim(bool).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Fish
+{
+public:
+  void Swim()
+  {
+    cout << "Fish Swims....!" << endl;
+  }
+  void Swim(bool FreshWaterFish)
+  {
+    if(FreshWaterFish)
+      cout << "Swims in lake" << endl;
+    else
+      cout << "Swims in sea" << endl;
+  }
+};
+
+class Tuna: public Fish
+{
+public:
+  void Swim()
+  {
+    cout << "Tuna swims real fast" << endl;
+  }
+};
+
+int main()
+{
+  Tuna myDinner;
+
+  cout << "Getting my food to swim" << endl;
+  // myDinner.Swim(false); // compile failure: Fish::Swim(bool) is hiddenby Tuna::Swim()
+  myDinner.Swim();
+
+  return 0;
+}
