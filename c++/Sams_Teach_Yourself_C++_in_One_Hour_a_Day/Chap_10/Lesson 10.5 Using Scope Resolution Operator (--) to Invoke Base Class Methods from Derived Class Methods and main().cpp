3343733a644b3232de03171987b01185ba6effce
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-05-31 18:22:09 +0100

    Lesson 10.5 Commit

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_10/Lesson 10.5 Using Scope Resolution Operator (--) to Invoke Base Class Methods from Derived Class Methods and main().cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+class Fish
+{
+private:
+  bool FreshWaterFish;
+public:
+  //Fish Constructor
+  Fish(bool IsFreshWater) : FreshWaterFish(IsFreshWater){}
+
+  void Swim()
+  {
+    if(FreshWaterFish)
+      cout << "Swims in lake" << endl;
+    else
+      cout << "Swims in sea" << endl;
+
+  }
+};
+
+class Tuna: public Fish
+{
+public:
+  Tuna(): Fish(false) {}
+
+  void Swim()
+  {
+    cout << "Tuna swims real fast" << endl;
+  }
+};
+
+class Carp: public Fish
+{
+public:
+  Carp():Fish(true) {}
+
+  void Swim()
+  {
+    cout << "Carp Swims real slow" << endl;
+    Fish::Swim();
+  }
+};
+
+int main()
+{
+  Carp myLunch;
+  Tuna myDinner;
+
+  cout << "Getting my food to swim" << endl;
+
+  cout << "Lunch: ";
+  myLunch.Swim();
+
+  cout << "Dinner: ";
+  myDinner.Fish::Swim();
+
+  return 0;
+}
