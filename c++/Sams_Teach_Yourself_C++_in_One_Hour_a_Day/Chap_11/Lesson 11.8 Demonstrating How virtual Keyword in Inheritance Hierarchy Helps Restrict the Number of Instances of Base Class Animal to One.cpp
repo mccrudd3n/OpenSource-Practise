577a2cfffda25c0e209f61f2e109d462b1fa0b24
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-06-09 23:32:18 +0100

    Lesson Commit

---
+++ c++/Sams_Teach_Yourself_C++_in_One_Hour_a_Day/Chap_11/Lesson 11.8 Demonstrating How virtual Keyword in Inheritance Hierarchy Helps Restrict the Number of Instances of Base Class Animal to One.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+public:
+  Animal()
+{
+  cout << "Animal constructor" << endl;
+}
+  // Sample Method
+  int Age;
+};
+
+class Mammal:public virtual Animal
+{
+};
+class Bird:public virtual Animal
+{
+};
+class Reptile:public virtual Animal
+{
+};
+
+class Platypus:public Mammal, public Bird, public Reptile
+{
+public:
+  Platypus()
+  {
+    cout << "Platypus constructor" << endl;
+  }
+};
+
+int main()
+{
+  Platypus duckBilledP;
+
+  // No complie error as there is only one Animal::Age
+  duckBilledP.Age = 25;
+
+  return 0;
+}
