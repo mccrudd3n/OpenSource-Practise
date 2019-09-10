a2817fdb0f1b353ba70f891e37d1b941b4e78141
mccrudd3n <49352991+mccrudd3n@users.noreply.github.com>
2019-09-10 14:19:57 +0100

    Lesson Commit

---
+++ Courses/MITx: 6.00.1x Introduction to Computer Science and Programming Using Python/Week 2: Simple Programs/Cube root.py
@@ -0,0 +1,15 @@
+## Finding the cude root of X tutorial
+cube = 27
+epsilon = 0.001
+guess = 0.0
+increment = 0.0001
+num_guesses = 0
+
+while abs(guess**3 - cube) >= epsilon:
+    guess += increment
+    num_guesses += 1
+print('num_guesses =', num_guesses)
+if abs(guess**3 -cube) >= epsilon:
+    print('Failed on cube root of', cube)
+else:
+    print(guess, 'is close to the cube root of', cube)
