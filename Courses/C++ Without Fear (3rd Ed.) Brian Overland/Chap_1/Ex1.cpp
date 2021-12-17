69b9296b120464f3d952f0eddd442c746173637d
John McCrudden <49352991+mccrudd3n@users.noreply.github.com>
2021-12-17 10:49:58 +0000

    C++ initial IDE testing

---
+++ Courses/C++ Without Fear (3rd Ed.) Brian Overland/Chap_1/Ex1.cpp
@@ -0,0 +1,10 @@
+// Ex 1~
+
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Statement 1, (with endl)" << endl;
+    cout << "Statement 2, (without endl) ";
+    cout << "Statement 3.";
+    return 0;
+}
